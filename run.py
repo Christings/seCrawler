64996b494b9186cda8c4edde50d17ed81fc1bd29
531404376@qq.com <531404376@qq.com>
2018-04-26 12:48:07 +0800

    update run

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 from scrapy import cmdline
 
-cmdline.execute("scrapy crawl keywordSpider -a keyword=农业企业走出去  -a se=baidu -a pages=2".split())
+cmdline.execute("scrapy crawl keywordSpider -a keyword=农业企业走出去问题 -a se=baidu -a pages=15".split())
